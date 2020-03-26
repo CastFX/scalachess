c75b6eadd8eacc00f5154e98d39da63e377244e4
maicol.forti <maicol.forti@studio.unibo.it>
2020-03-26 17:05:10 +0100

    Fix view visualization on prompt

--- client/src/test/scala/it/scalachess/view/test/ViewSpec.scala
+++ client/src/test/scala/it/scalachess/view/test/ViewSpec.scala
@@ -8,60 +8,21 @@ class ViewSpec extends FlatSpec with Matchers with Inspectors {
 
   val basicView    = ViewFactory(CLI)
   val board: Board = Board.defaultBoard()
-  val whitePawn    = '♙'
-  val blackPawn    = '♟'
-  val whiteRook    = '♖'
-  val blackRook    = '♜'
-  val whiteKnight  = '♘'
-  val blackKnight  = '♞'
-  val whiteBishop  = '♗'
-  val blackBishop  = '♝'
-  val whiteKing    = '♔'
-  val blackKing    = '♚'
-  val whiteQueen   = '♕'
-  val blackQueen   = '♛'
-
-  val piecesPerType: Map[Char, Int] = Map(
-    whitePawn   -> 8,
-    blackPawn   -> 8,
-    whiteRook   -> 2,
-    blackRook   -> 2,
-    whiteKnight -> 2,
-    blackKnight -> 2,
-    whiteBishop -> 2,
-    blackBishop -> 2,
-    whiteKing   -> 1,
-    blackKing   -> 1,
-    whiteQueen  -> 1,
-    blackQueen  -> 1
-  )
+  val outputView: String = s"8 [ r ][ n ][ b ][ q ][ k ][ b ][ n ][ r ]\n" +
+  s"7 [ p ][ p ][ p ][ p ][ p ][ p ][ p ][ p ]\n" +
+  s"6 [   ][   ][   ][   ][   ][   ][   ][   ]\n" +
+  s"5 [   ][   ][   ][   ][   ][   ][   ][   ]\n" +
+  s"4 [   ][   ][   ][   ][   ][   ][   ][   ]\n" +
+  s"3 [   ][   ][   ][   ][   ][   ][   ][   ]\n" +
+  s"2 [ P ][ P ][ P ][ P ][ P ][ P ][ P ][ P ]\n" +
+  s"1 [ R ][ N ][ B ][ Q ][ K ][ B ][ N ][ R ]\n" +
+  s"    A    B    C    D    E    F    G    H  "
 
   "A basic view" should "be an instance of BasicView" in {
     basicView equals CliView shouldEqual true
   }
 
-  it should
-  s"have ${piecesPerType(whitePawn)} white pawns, " +
-  s"${piecesPerType(blackPawn)} black pawns, " +
-  s"${piecesPerType(whiteRook)} white rooks, " +
-  s"${piecesPerType(blackRook)} black rooks, " +
-  s"${piecesPerType(whiteKnight)} white knights, " +
-  s"${piecesPerType(blackKnight)} black knights, " +
-  s"${piecesPerType(whiteBishop)} white bishops, " +
-  s"${piecesPerType(blackBishop)} black bishops, " +
-  s"${piecesPerType(whiteKing)} white kings, " +
-  s"${piecesPerType(blackKing)} black kings, " +
-  s"${piecesPerType(whiteQueen)} white queens" +
-  s"${piecesPerType(blackQueen)} black queens" in {
-
-    val piecesCounts = basicView
-      .getStringFromBoard(board)
-      .groupBy(identity)
-      .mapValues(_.length)
-      .toSet
-      .filter(x => piecesPerType.contains(x._1))
-
-    piecesCounts shouldEqual piecesPerType.toSet
+  it should "be visualized as" in {
+    basicView.getStringFromBoard(board) shouldEqual outputView
   }
-
 }
