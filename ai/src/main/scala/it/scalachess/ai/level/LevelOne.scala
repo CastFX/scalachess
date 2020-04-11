3b70878862353dfdcd30a3af6571f2b5bd1ea47f
deividvd <52574683+deividvd@users.noreply.github.com>
2020-04-11 09:51:49 +0200

    first rough implementation of level one

--- ai/src/main/scala/it/scalachess/ai/level/LevelOne.scala
+++ ai/src/main/scala/it/scalachess/ai/level/LevelOne.scala
@@ -2,11 +2,24 @@ package it.scalachess.ai.level
 import it.scalachess.core.Color
 import it.scalachess.core.board.Board
 import it.scalachess.core.logic.moves.FullMove
+import it.scalachess.core.logic.moves.generators.MoveGenerator
 import it.scalachess.core.pieces.{ Bishop, King, Knight, Pawn, PieceType, Queen, Rook }
 
 case class LevelOne() extends Level {
 
-  override def apply(board: Board, player: Color, history: Seq[FullMove]): FullMove = ???
+  override def apply(board: Board, player: Color, history: Seq[FullMove]): FullMove =
+    new MoveGenerator(board: Board, player: Color, history: Seq[FullMove])
+      .allMoves()
+      .map(move => evaluateBoardByPieceValue(board(move.validMove.boardChanges), player) -> move)
+      .toMap
+      .max
+      ._2
+
+  def evaluateBoardByPieceValue(board: Board, player: Color): Double =
+    board.pieces
+      .filter(_._2.color == player)
+      .map(piece => pieceValue(piece._2.pieceType))
+      .fold(0.0)(_ + _)
 
   def pieceValue(pieceType: PieceType): Double =
     pieceType match {
@@ -16,6 +29,7 @@ case class LevelOne() extends Level {
       case Rook   => rookValue
       case Queen  => queenValue
       case King   => kingValue
+      case _      => 0
     }
 
   // pieces' value estimated by Alan Turing
