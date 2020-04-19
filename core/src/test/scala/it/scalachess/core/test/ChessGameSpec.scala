484819b14fbaac51ce3bdb921a40a07306b6cf27
Christian Serra <chs@bendingspoon.com>
2020-04-19 21:04:44 +0200

    refactored tests

--- core/src/test/scala/it/scalachess/core/test/ChessGameSpec.scala
+++ core/src/test/scala/it/scalachess/core/test/ChessGameSpec.scala
@@ -1,6 +1,6 @@
 package it.scalachess.core.test
 
-import it.scalachess.core.{ Black, ChessGame, Draw, Ongoing, White, Win }
+import it.scalachess.core.{ Black, ChessGame, Draw, Ongoing, White, Win, WinByForfeit }
 import it.scalachess.core.board.{ Board, Position }
 import it.scalachess.core.logic.moves.{ FullMove, ValidSimpleMove }
 import it.scalachess.core.pieces.{ King, Knight, Pawn, Piece, Rook }
@@ -8,145 +8,93 @@ import org.scalatest.{ FlatSpec, Matchers, OptionValues }
 import it.scalachess.core.test.ChessGameFailureMatcher.generateFailure
 
 class ChessGameSpec extends FlatSpec with Matchers with OptionValues {
-  var simpleGame: ChessGame              = ChessGame.standard()
-  var testKingConstraintsGame: ChessGame = ChessGame.standard()
-  var foolMateGame: ChessGame            = ChessGame.standard()
-  var scholarMateGame: ChessGame         = ChessGame.standard()
-  var simpleGameTurnCounter              = 0
+  val standardGame: ChessGame = ChessGame.standard()
 
   "A standard ChessGame" should "have a non-empty board" in {
-    assert(simpleGame.board.pieces.nonEmpty)
+    standardGame.board.pieces shouldBe 'nonEmpty
   }
 
   it should "have a starting white player at turn 0" in {
-    simpleGame.player should equal(White)
-    simpleGame.turn should be(simpleGameTurnCounter)
+    standardGame.player should equal(White)
+    standardGame.turn shouldBe 0
   }
 
   it should "not accept a move with an illegal format" in {
-    val illegalFormatMove = "aa2 a3"
-    simpleGame(illegalFormatMove) should generateFailure
+    standardGame("aa2 a3") should generateFailure
   }
 
   it should "not allows player to move an opponent piece" in {
-    val blackPawnMove = "a7 a6"
-    simpleGame(blackPawnMove) should generateFailure
+    standardGame("a6") should generateFailure
   }
 
   it should "not allows a player captures his own pieces" in {
-    val queenCapturesPawn = "Qxd2"
-    simpleGame(queenCapturesPawn) should generateFailure
+    standardGame("Qxd2") should generateFailure
   }
 
   it should "not allows specific pieces pass through other pieces" in {
-    val queenPassThroughPawn = "Qd3"
-    simpleGame(queenPassThroughPawn) should generateFailure
+    val invalidWhiteMoves = Seq("Qd3", "Qd4", "Qh5", "Qa4", "Bh6", "Ba3", "Ra6", "Bh3", "Ba6", "Rh6")
+    invalidWhiteMoves.foreach(ChessGame.standard()(_) should generateFailure)
+
+    val blackMovesGame    = GameCreator.movesToGame(Seq("a3"))
+    val invalidBlackMoves = Seq("Ra3", "Ba6", "Bh3", "Qa5", "Qd5", "Qh4", "Ba3", "Bh6", "Rh6")
+    invalidBlackMoves.foreach(blackMovesGame(_) should generateFailure)
   }
 
   it should "apply a move correctly" in {
-    val whitePawnMove          = "a4"
-    val whitePawnFinalPosition = Position(1, 4)
-    simpleGame(whitePawnMove).toOption.value.board.pieceAtPosition(whitePawnFinalPosition).value should equal(
-      Piece(White, Pawn))
+    GameCreator.movesToGame(Seq("a4")).board.pieceAtPosition(Position(1, 4)).value shouldEqual Piece(White, Pawn)
   }
 
   it should "must alternate turns between players" in {
-    val whitePawnMove = "a3"
-    val blackPawnMove = "a6"
-    simpleGame = simpleGame(whitePawnMove).toOption.value
-    simpleGameTurnCounter += 1
-    simpleGame.turn should be(simpleGameTurnCounter)
-    simpleGame.player should equal(Black)
-    simpleGame = simpleGame(blackPawnMove).toOption.value
-    simpleGameTurnCounter += 1
-    simpleGame.turn should be(simpleGameTurnCounter)
-    simpleGame.player should equal(White)
+    val whiteMoved = GameCreator.movesToGame(Seq("a3"))
+    whiteMoved.turn shouldBe 1
+    whiteMoved.player shouldBe Black
+    val blackMoved = whiteMoved("a6").toOption.value
+    blackMoved.turn shouldBe 2
+    blackMoved.player shouldBe White
   }
 
   "Build a game where the king " should "not being able do some move because it's under check" in {
-    val whitePawnMove               = "e4"
-    val firstBlackPawnMove          = "e5"
-    val whiteBishopMove             = "Bd3"
-    val secondBlackPawnMove         = "d5"
-    val secondWhitePawnMove         = "f4"
-    val blackQueenMove              = "Qh4+"
-    val whiteKingMoveNotAllowed     = "Kf2"
-    val whiteKingMoveAllowed        = "Ke2"
-    val anotherWhiteKingMoveAllowed = "Kf1"
-    testKingConstraintsGame = testKingConstraintsGame(whitePawnMove).toOption.value
-    testKingConstraintsGame = testKingConstraintsGame(firstBlackPawnMove).toOption.value
-    testKingConstraintsGame = testKingConstraintsGame(whiteBishopMove).toOption.value
-    testKingConstraintsGame = testKingConstraintsGame(secondBlackPawnMove).toOption.value
-    testKingConstraintsGame = testKingConstraintsGame(secondWhitePawnMove).toOption.value
-    testKingConstraintsGame = testKingConstraintsGame(blackQueenMove).toOption.value
-    testKingConstraintsGame.isKingInCheck shouldBe true
-    testKingConstraintsGame.gameStatus should be(Ongoing)
-    testKingConstraintsGame(whiteKingMoveNotAllowed) should generateFailure
-    testKingConstraintsGame(whiteKingMoveAllowed).isSuccess should be(true)
-    testKingConstraintsGame = testKingConstraintsGame(anotherWhiteKingMoveAllowed).toOption.value
-    testKingConstraintsGame.isKingInCheck shouldBe false
+    val game = GameCreator.movesToGame(Seq("e4", "e5", "Bd3", "d5", "f4", "Qh4+"))
+    game.isKingInCheck shouldBe true
+    game.gameStatus shouldBe Ongoing
+    game("Kf2") should generateFailure
+    game("Ke2").isSuccess shouldBe true
+    game("Kf1").toOption.value.isKingInCheck shouldBe false
+  }
+
+  "Kasparov immortal" should "be played correctly" in {
+    GameCreator.kasparovImmortal.gameStatus shouldBe Ongoing
+    val blackForfeits = GameCreator.kasparovImmortal.end(WinByForfeit(White))
+    blackForfeits.gameStatus shouldBe WinByForfeit(White)
   }
 
   /*
    * FOOL'S MATE
    * */
   "Build a Fool's Check Mate in which the game " should " end in 4 turn," in {
-    val firstWhitePawnMove  = "f3"
-    val blackPawnMove       = "e6"
-    val secondWhitePawnMove = "g4"
-    val blackQueenMove      = "Qh4#"
-    foolMateGame = foolMateGame(firstWhitePawnMove).toOption.value
-    foolMateGame = foolMateGame(blackPawnMove).toOption.value
-    foolMateGame = foolMateGame(secondWhitePawnMove).toOption.value
-    foolMateGame.isKingInCheck shouldBe false
-    foolMateGame = foolMateGame(blackQueenMove).toOption.value
-    foolMateGame.isKingInCheck shouldBe true
-    foolMateGame.gameStatus should equal(Win(Black))
+    val game = GameCreator.movesToGame(Seq("f3", "e6", "g4"))
+    game.isKingInCheck shouldBe false
+    val foolsMate = game("Qh4#").toOption.value
+    foolsMate.isKingInCheck shouldBe true
+    foolsMate.gameStatus should equal(Win(Black))
   }
 
   /*
    * SCHOLAR'S MATE
    * */
   "Build a Scholar's Check Mate in which the game " should " end in 7 turn," in {
-    scholarMateGame = GameCreator.scholarGame
-    scholarMateGame.gameStatus should equal(Win(White))
-    scholarMateGame.isKingInCheck shouldBe true
+    GameCreator.scholarGame.gameStatus should equal(Win(White))
+    GameCreator.scholarGame.isKingInCheck shouldBe true
   }
 
   "Build a game where the white player" should "be able to use a QueenSide Castling" in {
-    var castlingGame: ChessGame = ChessGame.standard()
-    val firstMove               = "Na3"
-    val secondMove              = "a6"
-    val thirdMove               = "b3"
-    val fourthMove              = "b6"
-    val fifthMove               = "c3"
-    val sixthMove               = "c6"
-    val seventhMove             = "Qc2"
-    val eighthMove              = "d6"
-    val ninthMove               = "Bb2"
-    val tenthMove               = "e6"
-    val castling                = "0-0-0"
-    Seq(firstMove,
-        secondMove,
-        thirdMove,
-        fourthMove,
-        fifthMove,
-        sixthMove,
-        seventhMove,
-        eighthMove,
-        ninthMove,
-        tenthMove,
-        castling).foreach(move => castlingGame = castlingGame(move).toOption.value)
-    castlingGame.board.pieceAtPosition(Position(3, 1)).value shouldBe Piece(White, King)
-    castlingGame.board.pieceAtPosition(Position(4, 1)).value shouldBe Piece(White, Rook)
+    GameCreator.castlingQueenSideGame.board.pieceAtPosition(Position(3, 1)).value shouldBe Piece(White, King)
+    GameCreator.castlingQueenSideGame.board.pieceAtPosition(Position(4, 1)).value shouldBe Piece(White, Rook)
   }
 
   "Build a game where the black player" should "be able to use a KingSide Castling" in {
-    var castlingGame: ChessGame = ChessGame.standard()
-    Seq("a3", "Nh6", "b3", "g6", "c3", "f6", "d3", "Bg7", "e3", "0-0").foreach(move =>
-      castlingGame = castlingGame(move).toOption.value)
-    castlingGame.board.pieceAtPosition(Position(7, 8)).value shouldBe Piece(Black, King)
-    castlingGame.board.pieceAtPosition(Position(6, 8)).value shouldBe Piece(Black, Rook)
+    GameCreator.castlingKingSideGame.board.pieceAtPosition(Position(7, 8)).value shouldBe Piece(Black, King)
+    GameCreator.castlingKingSideGame.board.pieceAtPosition(Position(6, 8)).value shouldBe Piece(Black, Rook)
   }
 
   "At the start of a game, a castling" should "not be allowed" in {
@@ -156,8 +104,6 @@ class ChessGameSpec extends FlatSpec with Matchers with OptionValues {
   }
 
   "A correct move history" should "be created during a game" in {
-    var game: ChessGame = ChessGame.standard()
-
     val validMoves = Seq(
       ValidSimpleMove(Position(2, 1), Position(1, 3), Knight, White, None),
       ValidSimpleMove(Position(2, 7), Position(2, 6), Pawn, Black, None),
@@ -173,22 +119,16 @@ class ChessGameSpec extends FlatSpec with Matchers with OptionValues {
       FullMove(v, resultsInCheck = false, resultsInCheckmate = false, boardAfter)
     }
 
-    Seq("Na3", "b6", "d3", "d6", "g3").foreach(move => game = game(move).toOption.value)
+    val game = GameCreator.movesToGame(Seq("Na3", "b6", "d3", "d6", "g3"))
     game.moveHistory shouldEqual history
   }
 
   "A Pawn on Position h4" should "be able to capture on g5" in {
-    val game = Seq("h4", "g5", "hxg5").foldLeft(ChessGame.standard())((game, move) => game(move).toOption.value)
+    val game = GameCreator.movesToGame(Seq("h4", "g5", "hxg5"))
     game.board.pieces(Position(7, 5)).color shouldBe White
   }
 
   "A stalemate" should "result in a draw" in {
-    var drawGame = ChessGame.standard()
-    (Seq("e3", "a5", "Qh5", "Ra6", "Qxa5", "h5", "h4", "Rah6", "Qxc7", "f6")
-    ++ Seq("Qxd7+", "Kf7", "Qxb7", "Qd3", "Qxb8", "Qh7", "Qxc8", "Kg6", "Qe6"))
-      .foreach { move =>
-        drawGame = drawGame(move).toOption.value
-      }
-    drawGame.gameStatus shouldBe Draw
+    GameCreator.drawGame.gameStatus shouldBe Draw
   }
 }
